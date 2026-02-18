d1c55b9ba9114db6c5125f44e9e9edfb1d7bdf0e
Sangshrut <bsangshrut@gmail.com>
2026-02-18 16:41:50 +0000

    coded UC2 to print OOPS to console

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -5,11 +5,21 @@
  * Oriented Programming System OOPS acronym to the console.
  * 
  * @author Developer
- * @version 1.0
+ * @version 2.0
  */
 
 public class OOPSBannerApp {
     public static void main(String[] args) {
-        System.out.println("OOPS");
+
+        System.out.println("    ***        ***      ******     *****     ");
+        System.out.println("  **   **   **     **   **   **   **         ");
+        System.out.println(" **     **  **     **   **    ** **          ");
+        System.out.println(" **     **  **     **   **   **    **        ");
+        System.out.println(" **     **  **     **   ******      ***      ");
+        System.out.println(" **     **  **     **   **            **     ");
+        System.out.println(" **     **  **     **   **             **    ");
+        System.out.println("  **   **    **   **    **            **     ");
+        System.out.println("    ***        ***      **        *****       ");
+
     }
 }
